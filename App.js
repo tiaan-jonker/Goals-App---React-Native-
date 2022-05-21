04fa714588ce6cd51aacab4473429866acf41ab0
Tiaan Jonker <tj@Tiaans-MacBook-Pro.local>
2022-05-22 07:15:10 +1200

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Hello, Nicole!</Text>
+      <Text>I really love you ❤️</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  heading: {
+    fontSize: '32px',
+    color: 'blue'
+  }
+});
